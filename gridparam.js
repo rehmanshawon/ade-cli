ee7048c528fb146d0b3805e43cd341d5711f0be4
Rehman Shawon <rehman.shawon@apsissolutions.comm>
2022-11-17 17:03:55 +0600

    sys_masters altered

---
+++ gridparam.js
@@ -0,0 +1,82 @@
+gridParams={
+    slugName:"sys_users",
+    slugType:"grid",
+    queryTables:[
+    {
+    tableName:"sys_users",
+    fieldList:[
+        "user_name",         
+        "email",            
+    ],
+    columnList:[            
+            "User Name",	
+            "User Email",
+        ],            
+    },
+    {
+    tableName:"sys_roles",
+    fieldList:[
+        "role_name",                       
+    ],
+    columnList:[            
+            "Role Name",                
+        ],     
+    },
+]
+}
+
+gridParams={
+
+    slugName:"sys_users",
+
+    slugType:"grid",
+
+    queryTables:[
+
+    {
+
+    tableName:"sys_users",
+
+        fieldList:[
+
+            {
+
+            fieldName:"user_name",
+
+            columnName:"User Name",
+
+            },
+
+            {
+
+            fieldName:"email",
+
+            columnName:"User Email",    
+
+            },
+
+        ]
+
+    },
+
+    {
+
+    tableName:"sys_roles",
+
+        fieldList:[
+
+            {
+
+            fieldName:"role_name",
+
+            columnName:"User Name",
+
+            },      
+
+        ]
+
+    },
+
+]
+
+}
